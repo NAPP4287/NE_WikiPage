b16d4114f28b194b3f34b4b79328f0844f799004
Lee Na Eun <nanni4287@gmail.com>
2024-02-22 12:46:44 +0900

    [FE] feature: 위키 컨텐츠 저장 기능 및 엔터 기능 추가

--- src/page/WikiDetailPage.tsx
+++ src/page/WikiDetailPage.tsx
@@ -3,18 +3,37 @@ import {
   useNavigate,
   createSearchParams,
 } from "react-router-dom";
-import { useState, useEffect } from "react";
-// dummyData
-import { DWikiDetailContents } from "src/dummyData/wikiListData";
+import { useState, useEffect, SetStateAction } from "react";
+// recoil
+import { useRecoilState } from "recoil";
+import { allContentsState } from "src/recoil/stateList";
 // interface
 import { IWikiDetailContents } from "src/interface/interfaceWiki";
 
-const findWikiLink = (content?: string, moveAction?: any, isEdit?: boolean) => {
-  const splitContents = content?.split("*");
-  const findSameTitle = DWikiDetailContents.filter((el) =>
+const findWikiLink = (
+  detailContents?: IWikiDetailContents,
+  setDetailContents?: SetStateAction<any>,
+  moveAction?: any,
+  isEdit?: boolean,
+  allContents?: Array<IWikiDetailContents>,
+  saveContents?: Function | any,
+) => {
+  const splitContents = detailContents?.contents?.split("*");
+  const findSameTitle: any = allContents?.filter((el) =>
     splitContents?.includes(el.title),
   );
-  const moveIdx = findSameTitle[0]?.id;
+
+  const moveIdx = findSameTitle ? findSameTitle[0]?.id : "";
+
+  const editContent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDetailContents({ ...detailContents, contents: e.target.value });
+  };
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+      saveContents();
+    }
+  };
 
   return (
     <div>
@@ -32,7 +51,12 @@ const findWikiLink = (content?: string, moveAction?: any, isEdit?: boolean) => {
           </div>
         ))
       ) : (
-        <textarea>{content}</textarea>
+        <input
+          className=" w-full"
+          defaultValue={detailContents?.contents}
+          onChange={(e) => editContent(e)}
+          onKeyDown={handleKeyDown}
+        />
       )}
     </div>
   );
@@ -42,15 +66,18 @@ const WikiDetailPage = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const query = searchParams.get("id") || "0"; // query id
+
   const [detailContents, setDetailContents] = useState<IWikiDetailContents>();
   const [isEdit, setIsEdit] = useState<boolean>(false);
+
+  const [allContents, setAllContents] = useRecoilState(allContentsState);
 
   useEffect(() => {
     callDetailContents();
   }, [query]);
 
   const callDetailContents = () => {
-    const result = DWikiDetailContents.filter((el) => el.id === Number(query));
+    const result = allContents?.filter((el) => el.id === Number(query));
     setDetailContents({ ...result[0] });
   };
 
@@ -62,12 +89,34 @@ const WikiDetailPage = () => {
     });
   };
 
+  const saveContents = (): void => {
+    const saveIdx = allContents.findIndex((el) => String(el.id) === query);
+
+    const startArr = allContents.slice(0, saveIdx);
+    const endArr = allContents.slice(saveIdx + 1, allContents.length);
+    const changeArr: Array<IWikiDetailContents> | any = [
+      ...startArr,
+      detailContents,
+      ...endArr,
+    ];
+
+    setAllContents([...changeArr]);
+    setIsEdit(!isEdit);
+  };
+
   return (
     <div>
       <h3>{detailContents?.title}</h3>
-      {findWikiLink(detailContents?.contents, onMoveOtherWiki, isEdit)}
+      {findWikiLink(
+        detailContents,
+        setDetailContents,
+        onMoveOtherWiki,
+        isEdit,
+        allContents,
+        saveContents,
+      )}
       {isEdit ? (
-        <button onClick={() => setIsEdit(!isEdit)}>저장하기</button>
+        <button onClick={() => saveContents()}>저장하기</button>
       ) : (
         <button onClick={() => setIsEdit(!isEdit)}>수정하기</button>
       )}
--- src/recoil/stateList.ts
+++ src/recoil/stateList.ts
@@ -1,7 +1,28 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
+// interface
+import {
+  IWikiDetailContents,
+  IListContents,
+} from "src/interface/interfaceWiki";
+// dummyData
+import { DWikiList, DWikiDetailContents } from "src/dummyData/wikiListData";
 
 const { persistAtom } = recoilPersist({
   key: "importantState",
   storage: sessionStorage,
+});
+
+export const wikiListState = atom<Array<IListContents>>({
+  key: "wikiListState",
+  default: [...DWikiList],
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const allContentsState = atom<IWikiDetailContents[]>({
+  key: "allContents",
+
+  // default에는 임의의 데이터를 넣어줍시다.
+  default: [...DWikiDetailContents],
+  effects_UNSTABLE: [persistAtom],
 });
